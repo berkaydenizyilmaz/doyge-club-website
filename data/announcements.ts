f14f5a101aedf87622699830421a279a5a014421
Berkay Deniz Yılmaz <berkayd.2003@gmail.com>
2024-10-19 14:00:52 +0300

    yeni duyurular eklendi

--- data/announcements.ts
+++ data/announcements.ts
@@ -9,18 +9,23 @@ export interface AnnouncementType {
 
 export const announcements: AnnouncementType[] = [
   {
-    id: "2",
-    title: "Oyun Turnuvası",
+    id: "4",
+    title: "C Programlama Dili Eğitimi",
     content:
-      "DOYGE olarak düzenlediğimiz valorant oyun turnuvasına katılmayı unutmayın.",
-    link: "https://discord.gg/VUttf2VXeq",
-    linkText: "Başvurmak için tıklayın.",
+      "DOYGE ve Yazılım Geliştirme kulübü olarak vize öncesi C programlama dili eğitimi verilecektir.",
+    date: "23 Ekim Çarşamba 13.00",
   },
   {
     id: "3",
-    title: "Unreal Engine 5",
+    title: "Unreal Engine 5 Eğitimi",
     content:
-      "DOYGE olarak düzenlediğimiz Unreal Engine 5 eğitimine katılmayı unutmayın.",
+      "Unreal Engine 5 eğitimi verilecektir. Unreal Engine 5 hakkında bilgi sahibi olmak isteyenlerin katılımını bekliyoruz.",
+    date: "Yakında",
+  },
+  {
+    id: "4",
+    title: "Valorant Turnuvası",
+    content: "Valorant turnuvası düzenlenecektir.",
     date: "Yakında",
   },
 ];
